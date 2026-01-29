61b9139d258f9d8a1c85f1b5f3ae44704f181bf6
SamKynaston <Samuelkynno@gmail.com>
2026-01-29 14:58:45 +0000

    updated icon, improved styling

--- frontend/src/Components/Routes.tsx
+++ frontend/src/Components/Routes.tsx
@@ -1,6 +1,6 @@
 import type { Page } from "@bookwebapp/types";
 import Home from "../Pages/Home";
-import icon from "../../public/libraryicn.svg";
+import icon from "../../public/logo.svg";
 
 export const pages: Page[] = [
   { title: "Library", path: "/", element: <Home />, image: icon },
